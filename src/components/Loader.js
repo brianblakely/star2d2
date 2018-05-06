73ea8b5a675fb255f166df794b4e1be71e53a486
Brian <brian@Macintosh.local>
2018-05-06 01:26:05 -0400

    Catch HTTP errors and make sure loading joggers render above entire UI.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -12,18 +12,26 @@ export default class Loader extends React.Component {
   render() {
     const LoaderView = this.props.view;
 
-    if(!this.props.children) {
+    if(this.state.error && !this.state.quit) {
+      return (
+        <LoaderView loaded={true} error={true} />
+      );
+    }
+
+    if(!this.props.children && !this.state.quit) {
       return (
         <LoaderView loaded={this.state.loaded} />
       );
-    } else {
+    } else if(this.props.children) {
       return (
         <React.Fragment>
-          <LoaderView loaded={this.state.loaded} />
+          {!this.state.quit && <LoaderView loaded={this.state.loaded} />}
           {this.state.loaded && this.props.children(this.state)}
         </React.Fragment>
       );
     }
+
+    return null;
   }
 
   componentDidMount() {
@@ -32,6 +40,7 @@ export default class Loader extends React.Component {
         this.setState({
           loaded: true
         });
+        this.quit();
       });
     } else if(this.props.json && !this.state.loaded) {
       this.loadJSON();
@@ -41,13 +50,32 @@ export default class Loader extends React.Component {
   loadJSON() {
     fetch(this.props.json)
       .then((response)=> {
-        return response.json();
+        if(response.status === 200) {
+          return response.json();
+        } else {
+          return Promise.reject();
+        }
       })
       .then((data)=> {
         this.setState(Object.assign({
           loaded: true
         }, data));
+        this.quit();
+      }).catch(()=> {
+        this.setState({
+          loaded: false,
+          error: true
+        });
+        this.quit();
       });
+  }
+
+  quit() {
+    setTimeout(()=> {
+      this.setState({
+        quit: true
+      });
+    }, 1500);
   }
 }
 
--- src/components/LoaderView.js
+++ src/components/LoaderView.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import { CircularProgress } from 'material-ui/Progress';
+import red from 'material-ui/colors/red';
 import Tooltip from 'material-ui/Tooltip';
 
 const styles = ()=> ({
@@ -22,20 +24,28 @@ const styles = ()=> ({
 const LoaderView = (props)=> {
   const { classes } = props;
 
-  return (
-    <Tooltip
-      classes={{ popper: props.loaded ? classes.loaded : ``}}
-      title={props.loaded ? `Alright.` : `Juuuust a sec.`}
-      open
-    >
-      <CircularProgress className={`${classes.loader} ${props.loaded ? classes.loaded : ``}`} size={50} />
-    </Tooltip>
+  return ReactDOM.createPortal(
+    (
+      <Tooltip
+        classes={{ popper: props.loaded ? classes.loaded : ``}}
+        title={props.error ? `ERROR!!` : props.loaded ? `Alright.` : `Juuuust a sec.`}
+        open
+      >
+        <CircularProgress
+          className={`${classes.loader} ${props.loaded ? classes.loaded : ``}`}
+          size={50}
+          style={props.error && { color: red[500] }}
+        />
+      </Tooltip>
+    ),
+    document.body
   );
 };
 
 LoaderView.propTypes = {
   classes: PropTypes.object.isRequired,
-  loaded: PropTypes.bool
+  loaded: PropTypes.bool,
+  error: PropTypes.bool
 };
 
 export default withStyles(styles)(LoaderView);
